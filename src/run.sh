8b98eb1f356fc7c6cd04b8e39c3f2f6d1da3e4c7
yaofeng <yaof97@gmail.com>
2022-06-10 23:40:40 +0800

    Add DuEE Submission & Split Inference

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 torchrun \
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun \
     --nnodes=1 \
-    --nproc_per_node=7 \
+    --nproc_per_node=4 \
     EAE_main.py ./config/zh/eae/duee.yaml
