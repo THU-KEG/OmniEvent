aa7adcd33210eb6a4b73e2c3e574f539b383d423
h-peng17 <penghao20170136@163.com>
2022-09-06 15:29:48 +0800

    Fix bugs of ace-dygie processing

--- scripts/data_processing/ace2005-dygie/run.sh
+++ scripts/data_processing/ace2005-dygie/run.sh
@@ -1,5 +1,5 @@
-# pip install -r requirements.txt
-# python -m spacy download en_core_web_sm
-bash collect_ace_event.sh $1 
+pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+bash collect_ace_event.sh
 python parse_ace_event.py default-settings 
 python convert_to_openee.py 
