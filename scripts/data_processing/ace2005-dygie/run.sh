3b8992a362ea0cae2eb40061d9902ab57d89a15c
yaof20 <yaof97@gmail.com>
2023-10-15 00:09:00 -0700

    Add missing info for ace2005-dygie preprocessing

--- scripts/data_processing/ace2005-dygie/run.sh
+++ scripts/data_processing/ace2005-dygie/run.sh
@@ -1,8 +1,11 @@
-# # Install requirements
-# pip install -r requirements.txt
-# # Download scpay models
-# # We use en_core_web_sm==2.0.0 here: https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.0.0/en_core_web_sm-2.0.0.tar.gz
-# python -m spacy download en_core_web_sm
+# Create another conda environment for dygie processing (must use python 3.6)
+conda create -n dygie_process python=3.6
+conda activate dygie_process
+# Install requirements
+pip install -r requirements.txt
+# Download scpay models
+# We use en_core_web_sm==2.0.0 here: https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.0.0/en_core_web_sm-2.0.0.tar.gz
+python -m spacy download en_core_web_sm
 bash collect_ace_event.sh ../../../data/original/ace_2005_td_v7
 python parse_ace_event.py default-settings 
 python convert_to_openee.py
